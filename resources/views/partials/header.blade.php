ad081e8566cfb086741ab4404c531396696e22bf
pierfrancescoscar <scarinci401@gmail.com>
2022-01-19 15:47:13 +0100

    ADD: stylized header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,25 @@
-<header>
+<header class="p-3 header-bg-white header-shadow">
     <section class="container-fluid">
         <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 d-flex align-items-center">
                     {{-- Logo --}}
-                    <a href="/">Logo</a>
+                    <a href="{{ route('home') }}">
+                        <img src="{{ asset('images/dc-logo.png') }}" class="dc-logo" alt="">
+                    </a>
 
                     {{-- Navigation Menu --}}
                     <nav>
-                        <ul>
-                            <li>
-                                <a href="{{ route('home') }}">Comics</a>
-                                <a href="{{ route('news') }}">News</a>
+                        <ul class="d-flex mb-0">
+
+                            <li class="list-unstyled me-3">
+                                <a href="{{ route('home') }}" class="text-uppercase text-decoration-none dc-black">Comics</a>
                             </li>
+
+                            <li class="list-unstyled">
+                                <a href="{{ route('news') }}" class="text-uppercase text-decoration-none dc-black">News</a>
+                            </li>
+
                         </ul>
                     </nav>
                 </div>
